8e8fc1b58e8010834d8f9cb87fdfe4e82f4585c1
ronylitv14 <litvinets.rostislav@kneu.ua>
2024-01-29 13:59:09 +0200

    Minor changes in imports and for db interaction

--- handlers/admin_handler.py
+++ handlers/admin_handler.py
@@ -1,9 +1,8 @@
-from aiogram import Bot, F
+from aiogram import F
 from aiogram.enums import ChatType
-from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.dispatcher.router import Router
-from aiogram.filters.command import Command, CommandException
+from aiogram.filters.command import Command
 
 from aiogram_dialog.dialog import DialogManager
 from aiogram_dialog.api.entities import StartMode, ShowMode
@@ -13,9 +12,9 @@ from handlers.admin_panel.dialog_windows import create_admin_panel_dialog, creat
     create_users_dialog, create_balance_dialog, create_withdraw_dialog, create_ticket_dialog
 from handlers.admin_panel.window_states import AdminPanel
 from handlers.admin_panel.create_new_chat.dialog_windows import create_custom_chat_dialog
+from handlers.admin_panel.inactive_chat.dialog_windows import create_deactivation_chat_dialog
 
-from database.crud import get_user_auth
-from database.models import User, UserStatus
+from database_api.components.users import Users, UserStatus, UserResponse
 
 admin_panel_router = Router()
 
@@ -23,16 +22,14 @@ admin_panel_router.message.filter(F.chat.type.in_({ChatType.PRIVATE}))
 
 admin_panel_router.include_routers(create_admin_panel_dialog(), create_application_dialog(),
                                    create_users_dialog(), create_balance_dialog(), create_withdraw_dialog(),
-                                   create_regulations_dialog(), create_ticket_dialog(), create_custom_chat_dialog())
+                                   create_regulations_dialog(), create_ticket_dialog(), create_custom_chat_dialog(),
+                                   create_deactivation_chat_dialog())
 
 
-@admin_panel_router.message(
-    Command("panel")
-)
-async def run_admin_panel(message: Message, bot: Bot, state: FSMContext, dialog_manager: DialogManager):
-    user: User = await get_user_auth(message.from_user.id)
+async def start_admin_dialog(message: Message, dialog_manager: DialogManager):
+    user: UserResponse = await Users().get_user_from_db(message.from_user.id).do_request()
 
-    if not user:
+    if not isinstance(user, UserResponse):
         return await message.answer(text="Треба зареєструватись в системі!")
 
     if user.user_status not in [UserStatus.admin, UserStatus.superuser]:
@@ -48,3 +45,10 @@ async def run_admin_panel(message: Message, bot: Bot, state: FSMContext, dialog_
         mode=StartMode.RESET_STACK,
         show_mode=ShowMode.SEND
     )
+
+
+@admin_panel_router.message(
+    Command("panel")
+)
+async def run_admin_panel(message: Message, dialog_manager: DialogManager):
+    await start_admin_dialog(message, dialog_manager)
