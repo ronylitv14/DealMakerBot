9108615e675f97fd65d01f56d29df16bf4ca8d66
ronylitv14 <litvinets.rostislav@kneu.ua>
2024-01-29 15:38:42 +0200

    Minor changes for intreacting with db through API

--- handlers/utils/command_utils.py
+++ handlers/utils/command_utils.py
@@ -1,18 +1,20 @@
+from typing import Optional
+
 from aiogram import types
 from aiogram.fsm.context import FSMContext
 from keyboards.clients import create_keyboard_client
 from keyboards.keyboard_fields import ClientKeyboard
 
-from database.crud import get_user_auth
+from database_api.components.users import Users, UserResponse
 from handlers.states_handler import ClientDialog
 
 from utils.dialog_texts import auth_text
 
 
-async def show_menu(message: types.Message, state: FSMContext):
-    result = await get_user_auth(message.from_user.id)
+async def show_menu(message: types.Message, state: Optional[FSMContext] = None):
+    result: UserResponse = await Users().get_user_from_db(message.from_user.id).do_request()
 
-    if result:
+    if isinstance(result, UserResponse):
         await state.set_state(ClientDialog.client_state)
         await message.answer(
             text="<b>Обери наступну дію</b>",
@@ -20,6 +22,7 @@ async def show_menu(message: types.Message, state: FSMContext):
             parse_mode="HTML"
         )
     else:
+        await state.set_state(None)
         kbd = [
             [
                 types.KeyboardButton(text=ClientKeyboard.send_number, request_contact=True),
