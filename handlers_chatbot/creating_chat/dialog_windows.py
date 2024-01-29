d68f89002756a7e8179a70d744353f57074174b7
ronylitv14 <litvinets.rostislav@kneu.ua>
2024-01-29 13:50:41 +0200

    Updated dialog db interaction

--- handlers_chatbot/creating_chat/dialog_windows.py
+++ handlers_chatbot/creating_chat/dialog_windows.py
@@ -3,20 +3,19 @@ from typing import Any
 from aiogram_dialog.dialog import Dialog, DialogManager
 from aiogram_dialog.window import Window
 from aiogram_dialog.widgets.text import Format
-from aiogram_dialog.api.entities import LaunchMode
 
 from handlers_chatbot.creating_chat.dialog_states import ChatState
 from handlers_chatbot.creating_chat.dialog_widgets import input_message, btn_end_chat
-from handlers_chatbot.utils.redis_interaction import deactivate_session, is_session_active, activate_session, \
-    send_stored_messages, deactivate_all_unused_sessions
 
-from database.models import Chat
+from utils.redis_utils import deactivate_session, is_session_active, activate_session
+
+from database_api.components.chats import ChatModel
 
 
 async def on_chat_dialog_start(data: Any, dialog_manager: DialogManager):
     # TODO: Add sessions deactivation
 
-    chat_obj: Chat = dialog_manager.start_data.get("chat_obj")
+    chat_obj: ChatModel = dialog_manager.start_data.get("chat_obj")
     session_key = f"session:{chat_obj.client_id}:{chat_obj.id}"
 
     print(f"From func on_chat_dialog_start: {session_key}")
@@ -24,7 +23,7 @@ async def on_chat_dialog_start(data: Any, dialog_manager: DialogManager):
 
 
 async def on_chat_dialog_close(data: Any, dialog_manager: DialogManager, *args, **kwargs):
-    chat_obj: Chat = dialog_manager.start_data.get("chat_obj")
+    chat_obj: ChatModel = dialog_manager.start_data.get("chat_obj")
     session_key = f"session:{chat_obj.client_id}:{chat_obj.id}"
     is_active = await is_session_active(session_key)
     if is_active:
