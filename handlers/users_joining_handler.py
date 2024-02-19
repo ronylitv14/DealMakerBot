c2fad3314d30cf6b3bd93d642484dfcbc142059d
ronylitv14 <litvinets.rostislav@kneu.ua>
2024-02-19 21:08:20 +0200

    Added functionality for initiliazing data for lost chat

--- handlers/users_joining_handler.py
+++ handlers/users_joining_handler.py
@@ -2,11 +2,14 @@ import os
 from dotenv import load_dotenv
 
 from aiogram import F, Bot
-from aiogram.types import Message
+from aiogram.types import Message, ChatMemberOwner
+from aiogram.filters import Command
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, JOIN_TRANSITION, ChatMemberUpdated
 from aiogram.enums import ChatType
 from aiogram.dispatcher.router import Router
 
+from database_api.components.executors import Executors
+from database_api.components.tasks import TaskModel, Tasks
 from utils.dialog_texts import greetings_text
 from utils.redis_utils import compare_notification_time, set_notification_time, is_session_active
 from database_api.components.chats import Chats, ChatModel
@@ -79,3 +82,46 @@ async def check_for_new_messages(message: Message, bot: Bot):
             await set_notification_time(
                 int(db_chat_id)
             )
+
+
+@joining_users_router.message(
+    Command("initialize")
+)
+async def initialize_chat_data(message: Message, bot: Bot):
+    chat_desc = message.chat.description
+    _, task_id = chat_desc.split("№")
+
+    task: TaskModel = await Tasks().get_task_data(int(task_id)).do_request()
+
+    try:
+        chat_admin = "Random"
+        executor_id = None
+        member_count = await bot.get_chat_member_count(chat_id=message.chat.id)
+
+        executors = await Executors().get_all_executors().do_request()
+
+        for executor in executors:
+            is_chat_user = await message.chat.get_member(executor.telegram_id)
+            if is_chat_user:
+                executor_id = executor.telegram_id
+                break
+
+        for member in message.chat.get_administrators():
+            if isinstance(member, ChatMemberOwner):
+                chat_admin = member.user.username
+                break
+
+        await Chats().save_chat_data(
+            chat_id=message.chat.id,
+            task_id=task.task_id,
+            chat_admin=chat_admin,
+            client_id=task.client_id,
+            executor_id=executor_id,
+            group_name=message.chat.title,
+            invite_link=message.chat.invite_link,
+            participants_count=member_count
+        ).do_request()
+
+    except Exception as err:
+        print(err)
+        await message.answer("При новому ініціалізуванню чату сталась помилка!")
